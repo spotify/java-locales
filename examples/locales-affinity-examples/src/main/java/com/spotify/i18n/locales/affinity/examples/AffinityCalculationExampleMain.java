dfed7e89c22ca3fe55ba0579194dee737bb24825
Eric Fjøsne <efjosne@spotify.com>
2025-04-10 13:30:40 +0200

    chore: Add affinity example project structure

---
+++ examples/locales-affinity-examples/src/main/java/com/spotify/i18n/locales/affinity/examples/AffinityCalculationExampleMain.java
@@ -0,0 +1,14 @@
+
+package com.spotify.i18n.locales.affinity.examples;
+
+import java.io.IOException;
+
+/**
+ * Showcase implementation of Java-locales affinity calculation
+ *
+ * @author Eric Fjøsne
+ */
+public class AffinityCalculationExampleMain {
+
+  public static void main(String[] args) throws IOException {}
+}
---
+++ examples/locales-affinity-examples/src/main/java/com/spotify/i18n/locales/affinity/examples/ReferenceLocalesBasedJoinExampleMain.java
@@ -0,0 +1,14 @@
+
+package com.spotify.i18n.locales.affinity.examples;
+
+import java.io.IOException;
+
+/**
+ * Showcase implementation of Java-locales affinity calculation
+ *
+ * @author Eric Fjøsne
+ */
+public class ReferenceLocalesBasedJoinExampleMain {
+
+  public static void main(String[] args) throws IOException {}
+}
